5080994184d2b4821942aa8c717289642bae2969
PierreMATHIEU <p.mathieu742@gmail.com>
2017-03-06 14:29:35 +0100

    Initial commit

---
+++ app/src/main/java/mathieu/com/outerspacemanager/outerspacemanager/SignUpActivity.java
@@ -0,0 +1,11 @@
+package mathieu.com.outerspacemanager.outerspacemanager;
+
+import android.app.Activity;
+
+/**
+ * Created by Piou on 06/03/2017.
+ */
+
+public class SignUpActivity extends Activity {
+
+}
